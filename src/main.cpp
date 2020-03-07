eacb2b1c32cd2e049296031fbab5b1bb04ab7ff2
Johnny Borg <johnnyborg@hotmail.com>
2020-03-07 16:05:46 +0100

    minor tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
-//Libraries
+// Libraries
 #include <DHT.h>
 #include <LiquidCrystal.h>
 
-//Constants
-#define DHTPIN 2     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+// Constants
+#define DHTPIN 2          // what pin we're connected to
+#define DHTTYPE DHT22     // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
 
-//Variables
-float hum;  //Stores humidity value
-float temp; //Stores temperature value
+// Variables
+float hum = 0.0;    //Stores humidity value
+float temp = 0.0;   //Stores temperature value
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 void setup()
@@ -20,20 +20,20 @@ void setup()
 
 void loop()
 {
-    //Read data and store it to variables hum and temp
+    // Read data and store it to variables hum and temp
     hum = dht.readHumidity();
     temp= dht.readTemperature();
 
-    //Print temp and humidity values to serial monitor
+    // Print temp and humidity values to serial monitor
     lcd.setCursor(0, 0);
-    lcd.print("Humidity: ");
-    lcd.print(hum);
-    lcd.print("%");
-
-    lcd.setCursor(0, 1);
     lcd.print("Temp: ");
     lcd.print(temp);
     lcd.print(" C");
 
-    delay(10000);
+    lcd.setCursor(0, 1);
+    lcd.print("Humidity: ");
+    lcd.print(hum);
+    lcd.print("%");
+
+    delay(1000);
 }
